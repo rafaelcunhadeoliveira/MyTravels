d83137088ff8c3b61b17bf575096e9d7da77a547
Rafael Cunha <rafael_ptc@hotmail.com>
2018-10-31 18:49:29 -0300

    layout

--- MyTravels/View/ViewControllers/MapViewController.swift
+++ MyTravels/View/ViewControllers/MapViewController.swift
@@ -11,8 +11,39 @@ import MapKit
 
 class MapViewController: UIViewController {
 
+    @IBOutlet weak var editingView: UIView!
     @IBOutlet weak var map: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.configPinWhenTouch()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.isEditing = false
+        editingView.isHidden = true
+    }
+
+    func configPinWhenTouch() {
+        let tap = UILongPressGestureRecognizer(target: self, action: #selector(self.handleTap(gestureReconizer:)))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+
+    @IBAction func editPins(_ sender: Any) {
+        editingView.isHidden = self.isEditing
+        self.isEditing = !self.isEditing
+    }
+    
+    
+    @objc func handleTap(gestureReconizer: UILongPressGestureRecognizer) {
+        
+        let location = gestureReconizer.location(in: map)
+        let coordinate = map.convert(location,toCoordinateFrom: map)
+        
+        // Add annotation:
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+        map.addAnnotation(annotation)
     }
 }
